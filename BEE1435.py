1014c34143e04a9839add9f832838f4cccbe3cdb
rahulroynipon <rahulroynipon@gmail.com>
2024-05-12 19:52:35 +0600

    ad

---
+++ BEE1435.py
@@ -0,0 +1,14 @@
+def Matrix(n):
+    for i in range(n):
+        for j in range(n):
+            if (i==0 or j==0 or i==n-1 or j==n-1):
+            
+    
+
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        Matrix(n)
